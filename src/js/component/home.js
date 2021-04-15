e0d2418248b961c73c48e8d2e4934863ad8a9137
achaves100 <adan.chaves@outlook.com>
2021-04-15 00:38:17 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,41 @@
-import React from "react";
+// Adan - Import & Export: explicar uso en Home e Index
+// Adan - Porque la informacion va en Home y no en Index
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
-//create your first component
 export function Home() {
+	const [task, setTask] = useState(""); //Use state entrega 2 cosas: 1 variable y 1 funcion que modifica esa variable
+	const [list, setList] = useState([]);
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>TODO LIST</h1>
+			<p>{JSON.stringify(task)}</p>
+			<p>{JSON.stringify(list)}</p>
+			<div>
+				<form
+					onSubmit={e => {
+						e.preventDefault();
+						setList(list.concat(task));
+						setTask("");
+					}}>
+					<input
+						type="text"
+						value={task}
+						placeholder="Ingrese sus tareas"
+						onChange={e => {
+							//if (e.key == "Enter") {
+							//	setList(list.concat(task));
+							setTask(e.target.value);
+							//	console.log({ list });
+							//}
+						}}
+					/>
+				</form>
+				{list.map((item, index) => {
+					return <li key={index}>{item}</li>;
+				})}
+			</div>
 		</div>
 	);
 }
